615c01002d53397f1ad10b19381ba59dfe43245e
Eduardo Nery <eduardo.nery8@gmail.com>
2025-02-24 16:53:58 +0000

    Add pre-commit script that enforces lynting

--- back-end/main.py
+++ back-end/main.py
@@ -1,13 +1,14 @@
 import logging
 import os
+from typing import Dict, List
+
 import uvicorn
 from dotenv import load_dotenv
-from fastapi import FastAPI, Query, status, HTTPException
-from google.cloud import bigquery
-from langchain.schema import SystemMessage, HumanMessage, AIMessage
-from langchain_google_vertexai import ChatVertexAI
-from typing import List, Dict
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from google.cloud import bigquery
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain_google_vertexai import ChatVertexAI
 from pydantic import BaseModel
 
 load_dotenv()
@@ -20,30 +21,36 @@ else:
 app = FastAPI()
 
 app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["http://localhost:3000"],  
-        allow_credentials=True,
-        allow_methods=["*"],  # Allow all HTTP methods
-        allow_headers=["*"],  # Allow all headers
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
 )
 
 bq_client = bigquery.Client()
 llm = ChatVertexAI(model="gemini-1.5-flash-001")
 
-def generate_response(question: str, 
+
+def generate_response(question: str,
                       stackoverflow_results: List[Dict[str, str]]
-) -> AIMessage:
-    context = "\n".join([f"Answer: {q['body']}" for q in stackoverflow_results])
-    
+                      ) -> AIMessage:
+    context = "\n".join(
+        [f"Answer: {q['body']}" for q in stackoverflow_results])
+
     messages = [
-        SystemMessage(content=f"You are an AI assistant that answers programming questions using Stack Overflow data.\nHere are some Stack Overflow answers:\n'{context}'"),
-        HumanMessage(content=f"User asked: '{question}'\nProvide a concise and accurate response. If the question isn't programming related, tell the User to make one.")
-    ]
-    
-    return llm(messages)
+        SystemMessage(
+            content=f"You are an AI assistant that answers programming questio\
+                    ns using Stack Overflow data.\nHere are some Stack Overflo\
+                    w answers:\n'{context}'"), HumanMessage(
+            content=f"User asked: '{question}'\nProvide a concise and accurate\
+                     response. If the question isn't programming related, tell\
+                     the User to make one.")]
+
+    return llm.invoke(messages)
 
 
-def query(query: str, limit = 5) -> List[Dict[str, str]]:
+def query(query: str, limit=5) -> List[Dict[str, str]]:
     sql = f"""
     SELECT body
     FROM `bigquery-public-data.stackoverflow.posts_answers`
@@ -59,8 +66,10 @@ def query(query: str, limit = 5) -> List[Dict[str, str]]:
     logging.debug(f"Query executed: {sql}\nResults: {results}")
     return results
 
+
 class Question(BaseModel):
     question: str
+
 
 @app.post("/ask")
 async def post_ask(question_request: Question):
@@ -68,14 +77,17 @@ async def post_ask(question_request: Question):
     try:
         logging.debug(question)
         query_res = query(question)
-        #query_res = list(map(lambda row: {"body": row["body"]}, query_res))
+        # query_res = list(map(lambda row: {"body": row["body"]}, query_res))
         logging.info(f"Decoded results: {query_res}")
         response = generate_response(question, query_res)
         logging.info(f"Generated reponse: {response.content}")
         return {"answer": response.content}
     except Exception as e:
         logging.exception(str(e))
-        raise HTTPException({"error": "Unable to process question at this time."}, status_code=500)
+        raise HTTPException(
+            {"error": "Unable to process question at this time."},
+            status_code=500
+        )
 
-if __name__=="__main__":
+if __name__ == "__main__":
     uvicorn.run(app, port=8080)
--- back-end/test_main.py
+++ back-end/test_main.py
@@ -1,5 +1,7 @@
 import unittest
+
 from fastapi.testclient import TestClient
+
 import main
 from main import app
 
@@ -13,17 +15,25 @@ class TestMain(unittest.TestCase):
         # This is an online test as the method is simple
         query = "how to install pip"
         exp_result = [{
-  "body": "\u003cp\u003eI recommend you to uninstall python and then reinstall it again. In the installation window, use custom installation and check all the option which includes pip and also check to add pip to your environment variables.\u003c/p\u003e"
-}, {
-  "body": "\u003cp\u003eYou can use the wheel directly like this:\u003c/p\u003e\n\n\u003cpre\u003e\u003ccode\u003epython3 ./pip-19.2.2-py2.py3-none-any.whl/pip install ./pip-19.2.2-py2.py3-none-any.whl\n\u003c/code\u003e\u003c/pre\u003e"
-}]  
+            "body": "\u003cp\u003eI recommend you to uninstall python and the\
+                    n reinstall it again. In the installation window, use cus\
+                    tom installation and check all the option which includes \
+                    pip and also check to add pip to your environment variabl\
+                    es.\u003c/p\u003e"
+        }, {
+            "body": "\u003cp\u003eYou can use the wheel directly like this:\
+                    \u003c/p\u003e\n\n\u003cpre\u003e\u003ccode\u003epython3\
+                    ./pip-19.2.2-py2.py3-none-any.whl/pip install ./pip-19.2\
+                    .2-py2.py3-none-any.whl\n\u003c/code\u003e\u003c/pre\u003e"
+        }]
         res = main.query(query)
         self.assertEqual(res, exp_result)
 
     def test_post_ask(self):
-        response = self.client.post("/ask", json={"question": "How to install numpy?"})
+        response = self.client.post("/ask",
+                                    json={"question": "How to install numpy?"})
         self.assertEqual(response.status_code, 200)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     unittest.main()
